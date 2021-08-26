4005379adb8544809d6cf2a35be25b02f2b1fb32
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-08-26 16:16:27 +0900

    test(ResponseTest): add error response test

--- test/ResponseTest.cpp
+++ test/ResponseTest.cpp
@@ -19,8 +19,54 @@ TEST_F(ResponseTest, Version) {
   ExpectVersion(res, "1.1");
 }
 
+// autoindex test
 TEST_F(ResponseTest, AutoIndex) {
   Response res;
   res.AutoIndexResponse("./");
   EXPECT_TRUE(std::regex_match(res.GetBody(), std::regex("(.|\n)*")));
+  EXPECT_TRUE(res.GetStatusCode() == 200);
+  EXPECT_TRUE(res.GetStatusMessage() == "OK");
+  EXPECT_TRUE(res.GetVersion() == "1.1");
+  EXPECT_TRUE(res.GetHeader("Content-Type") == "text/html");
+}
+
+// Error Response test
+TEST_F(ResponseTest, Response404) {
+  Response res;
+  res.ErrorResponse(404);
+  EXPECT_TRUE(std::regex_match(res.GetBody(), std::regex("(.|\n)*")));
+  EXPECT_TRUE(res.GetStatusCode() == 404);
+  EXPECT_TRUE(res.GetStatusMessage() == "Not Found");
+  EXPECT_TRUE(res.GetVersion() == "1.1");
+  EXPECT_TRUE(res.GetHeader("Content-Type") == "text/html");
+}
+
+TEST_F(ResponseTest, Response405) {
+  Response res;
+  res.ErrorResponse(405);
+  EXPECT_TRUE(std::regex_match(res.GetBody(), std::regex("(.|\n)*")));
+  EXPECT_TRUE(res.GetStatusCode() == 405);
+  EXPECT_TRUE(res.GetStatusMessage() == "Method Not Allowd");
+  EXPECT_TRUE(res.GetVersion() == "1.1");
+  EXPECT_TRUE(res.GetHeader("Content-Type") == "text/html");
+}
+
+TEST_F(ResponseTest, Response502) {
+  Response res;
+  res.ErrorResponse(502);
+  EXPECT_TRUE(std::regex_match(res.GetBody(), std::regex("(.|\n)*")));
+  EXPECT_TRUE(res.GetStatusCode() == 502);
+  EXPECT_TRUE(res.GetStatusMessage() == "Bad Gateway");
+  EXPECT_TRUE(res.GetVersion() == "1.1");
+  EXPECT_TRUE(res.GetHeader("Content-Type") == "text/html");
+}
+
+TEST_F(ResponseTest, Response503) {
+  Response res;
+  res.ErrorResponse(503);
+  EXPECT_TRUE(std::regex_match(res.GetBody(), std::regex("(.|\n)*")));
+  EXPECT_TRUE(res.GetStatusCode() == 503);
+  EXPECT_TRUE(res.GetStatusMessage() == "Service Unavailable");
+  EXPECT_TRUE(res.GetVersion() == "1.1");
+  EXPECT_TRUE(res.GetHeader("Content-Type") == "text/html");
 }
