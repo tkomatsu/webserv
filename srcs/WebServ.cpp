56298385fada1b3eaf4dccd1b7e1aad09d115f07
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-09-02 20:47:16 +0900

    refactor(Webserv): change code order to be the same as header file

--- srcs/WebServ.cpp
+++ srcs/WebServ.cpp
@@ -27,22 +27,91 @@ WebServ::~WebServ() {
   sockets_.clear();
 }
 
-void WebServ::ParseConfig(const std::string &path) {
-  config::Parser parser(path);
+void WebServ::Activate(void) {
+  int hit = 0;
+  int n;
 
-  std::vector<config::Config> configs = parser.GetConfigs();
-  if (configs.empty()) return;
-
-  std::vector<config::Config>::const_iterator itr;
-  for (itr = configs.begin(); itr != configs.end(); ++itr) {
-    Server *server = new Server(*itr);
-    int fd = server->OpenListenSocket();
-    sockets_[fd] = server;
+  while (true) {
+    if ((n = HasUsableIO()) <= 0) throw std::runtime_error("select error\n");
+    for (std::map<int, Socket *>::iterator it = sockets_.begin();
+         n && it != sockets_.end(); ++it) {
+      try {
+        if (dynamic_cast<Server *>(it->second)) {
+          if (AcceptSession(it) == 0) --n;
+        } else {
+          if ((hit = ExecClientEvent(it)) > 0) n -= hit;
+        }
+      } catch (ft::HttpResponseException &e) {
+        Client *client = dynamic_cast<Client *>(it->second);
+        client->HandleException(e.what());
+      }
+    }
   }
 }
 
+int WebServ::HasUsableIO() {
+  int n = 0;
+
+  while (n == 0) {
+    FD_ZERO(&rfd_set_);
+    FD_ZERO(&wfd_set_);
+    max_fd_ = 0;
+
+    for (std::map<int, Socket *>::iterator it = sockets_.begin();
+         it != sockets_.end(); ++it) {
+      // set listening sockets_
+      if (dynamic_cast<Server *>(it->second)) {
+        int serevr_fd = it->first;
+
+        FD_SET(serevr_fd, &rfd_set_);
+        max_fd_ = std::max(max_fd_, serevr_fd);
+      } else {
+        int client_fd = it->first;
+        Client *client = dynamic_cast<Client *>(sockets_[client_fd]);
+
+        switch (client->GetStatus()) {
+          case READ_CLIENT:
+            FD_SET(client_fd, &rfd_set_);
+            max_fd_ = std::max(max_fd_, client_fd);
+            break;
+
+          case WRITE_CLIENT:
+            FD_SET(client_fd, &wfd_set_);
+            max_fd_ = std::max(max_fd_, client_fd);
+            break;
+
+          case READ_FILE:
+            FD_SET(client->GetReadFd(), &rfd_set_);
+            max_fd_ = std::max(max_fd_, client->GetReadFd());
+            break;
+
+          case WRITE_FILE:
+            FD_SET(client->GetWriteFd(), &wfd_set_);
+            max_fd_ = std::max(max_fd_, client->GetWriteFd());
+            break;
+
+          case READ_CGI:
+            FD_SET(client->GetReadFd(), &rfd_set_);
+            max_fd_ = std::max(max_fd_, client->GetReadFd());
+            break;
+
+          case READ_WRITE_CGI:
+            FD_SET(client->GetReadFd(), &rfd_set_);
+            FD_SET(client->GetWriteFd(), &wfd_set_);
+            max_fd_ = std::max(
+                max_fd_, std::max(client->GetWriteFd(), client->GetReadFd()));
+            break;
+        }
+      }
+    }
+
+    n = select(max_fd_ + 1, &rfd_set_, &wfd_set_, NULL, &timeout_);
+  }
+
+  return n;
+}
+
 int WebServ::AcceptSession(socket_iter it) {
-  int accepted = -1;
   int server_fd = it->first;
 
   if (FD_ISSET(server_fd, &rfd_set_)) {
@@ -54,9 +123,9 @@ int WebServ::AcceptSession(socket_iter it) {
     if (client_fd > max_fd_) max_fd_ = client_fd;
     sockets_[client_fd] = client;
 
-    accepted = 1;
+    return 0;
   }
-  return accepted;
+  return -1;
 }
 
 void WebServ::ReadClient(socket_iter it) {
@@ -122,68 +191,6 @@ void WebServ::WriteCGI(socket_iter it) {
   client->WriteCGIin();
 }
 
-int WebServ::HasUsableIO() {
-  int n = 0;
-
-  while (n == 0) {
-    FD_ZERO(&rfd_set_);
-    FD_ZERO(&wfd_set_);
-    max_fd_ = 0;
-
-    for (std::map<int, Socket *>::iterator it = sockets_.begin();
-         it != sockets_.end(); ++it) {
-      // set listening sockets_
-      if (dynamic_cast<Server *>(it->second)) {
-        int serevr_fd = it->first;
-
-        FD_SET(serevr_fd, &rfd_set_);
-        max_fd_ = std::max(max_fd_, serevr_fd);
-      } else {
-        int client_fd = it->first;
-        Client *client = dynamic_cast<Client *>(sockets_[client_fd]);
-
-        switch (client->GetStatus()) {
-          case READ_CLIENT:
-            FD_SET(client_fd, &rfd_set_);
-            max_fd_ = std::max(max_fd_, client_fd);
-            break;
-
-          case WRITE_CLIENT:
-            FD_SET(client_fd, &wfd_set_);
-            max_fd_ = std::max(max_fd_, client_fd);
-            break;
-
-          case READ_FILE:
-            FD_SET(client->GetReadFd(), &rfd_set_);
-            max_fd_ = std::max(max_fd_, client->GetReadFd());
-            break;
-
-          case WRITE_FILE:
-            FD_SET(client->GetWriteFd(), &wfd_set_);
-            max_fd_ = std::max(max_fd_, client->GetWriteFd());
-            break;
-
-          case READ_CGI:
-            FD_SET(client->GetReadFd(), &rfd_set_);
-            max_fd_ = std::max(max_fd_, client->GetReadFd());
-            break;
-
-          case READ_WRITE_CGI:
-            FD_SET(client->GetReadFd(), &rfd_set_);
-            FD_SET(client->GetWriteFd(), &wfd_set_);
-            max_fd_ = std::max(
-                max_fd_, std::max(client->GetWriteFd(), client->GetReadFd()));
-            break;
-        }
-      }
-    }
-
-    n = select(max_fd_ + 1, &rfd_set_, &wfd_set_, NULL, &timeout_);
-  }
-
-  return n;
-}
-
 int WebServ::ExecClientEvent(socket_iter it) {
   int hit = 0;
   int client_fd = it->first;
@@ -227,24 +234,16 @@ int WebServ::ExecClientEvent(socket_iter it) {
   return hit;
 }
 
-void WebServ::Activate(void) {
-  int hit = 0;
-  int n;
+void WebServ::ParseConfig(const std::string &path) {
+  config::Parser parser(path);
 
-  while (true) {
-    if ((n = HasUsableIO()) <= 0) throw std::runtime_error("select error\n");
-    for (std::map<int, Socket *>::iterator it = sockets_.begin();
-         n && it != sockets_.end(); ++it) {
-      try {
-        if (dynamic_cast<Server *>(it->second)) {
-          if (AcceptSession(it) == 1) --n;
-        } else {
-          if ((hit = ExecClientEvent(it)) > 0) n -= hit;
-        }
-      } catch (ft::HttpResponseException &e) {
-        Client *client = dynamic_cast<Client *>(it->second);
-        client->HandleException(e.what());
-      }
-    }
+  std::vector<config::Config> configs = parser.GetConfigs();
+  if (configs.empty()) return;
+
+  std::vector<config::Config>::const_iterator itr;
+  for (itr = configs.begin(); itr != configs.end(); ++itr) {
+    Server *server = new Server(*itr);
+    int fd = server->OpenListenSocket();
+    sockets_[fd] = server;
   }
 }
