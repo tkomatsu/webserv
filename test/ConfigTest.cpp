d6c1137168652e6b71a0944703e589fc150c521b
fkymy <yuskefuku@gmail.com>
2021-09-08 19:51:04 +0900

    test(test): add unit tests for LineBuilder

--- test/ConfigTest.cpp
+++ test/ConfigTest.cpp
@@ -9,7 +9,7 @@ class LineBuilderTest : public testing::Test {
 };
 
 TEST(LineBuilderTest, TestEmpty) {
-  std::ifstream file("../../conf/empty.conf");
+  std::ifstream file("../../conf/tests/empty.conf");
   config::LineBuilder builder(file);
   config::LineComponent line;
 
@@ -20,7 +20,7 @@ TEST(LineBuilderTest, TestEmpty) {
 }
 
 TEST(LineBuilderTest, TestWhitespace) {
-  std::ifstream file("../../conf/whitespaces.conf");
+  std::ifstream file("../../conf/tests/whitespaces.conf");
   config::LineBuilder builder(file);
   config::LineComponent line;
 
@@ -30,13 +30,250 @@ TEST(LineBuilderTest, TestWhitespace) {
   EXPECT_EQ(line.type, config::NONE);
 }
 
-class ConfigTest : public testing::Test {
- protected:
-  virtual void SetUp() {}
-  virtual void TearDown() {}
-};
+TEST(LineBuilderTest, TestComments) {
+  std::ifstream file("../../conf/tests/comments.conf");
+  config::LineBuilder builder(file);
+  config::LineComponent line;
 
-TEST(ConfigTest, TestTest) {
-  config::Parser parser("../../conf/default.conf");
-  std::vector<config::Config> configs = parser.GetConfigs();
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "print_config");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "on");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "index");
+  EXPECT_EQ(line.params.size(), 3);
+  EXPECT_EQ(line.params[0], "index.html#");
+  EXPECT_EQ(line.params[1], "autoindex");
+  EXPECT_EQ(line.params[2], "on");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "server");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_START);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "server");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_START);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "listen");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "8000");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "server_name");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "server1.com");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "location");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "/");
+  EXPECT_EQ(line.type, config::BLOCK_START);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "alias");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "/var/www/html/");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "allowed_methods");
+  EXPECT_EQ(line.params.size(), 3);
+  EXPECT_EQ(line.params[0], "GET");
+  EXPECT_EQ(line.params[1], "XXX");
+  EXPECT_EQ(line.params[2], "YYY");
+  EXPECT_EQ(line.type, config::SIMPLE);
+  
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_END);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_END);
+
+  EXPECT_FALSE(builder.GetNext(line));
+}
+
+TEST(LineBuilderTest, TestIndents) {
+  std::ifstream file("../../conf/tests/indents.conf");
+  config::LineBuilder builder(file);
+  config::LineComponent line;
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "what");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "what");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "what");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "what");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "server"); 
+  EXPECT_EQ(line.type, config::BLOCK_START);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "index");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "what.html#");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, ""); 
+  EXPECT_EQ(line.type, config::BLOCK_END);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "server"); 
+  EXPECT_EQ(line.type, config::BLOCK_START);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "listen");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "8001");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "root");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "/var/www/html");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "location");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "/test/");
+  EXPECT_EQ(line.type, config::BLOCK_START);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "index");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "index.html");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_END);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "location");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "/images/");
+  EXPECT_EQ(line.type, config::BLOCK_START);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_END);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_END);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "index");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "none.html");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "server");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_START);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "listen");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "8002");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_END);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "server");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_START);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "listen");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "8003");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "index");
+  EXPECT_EQ(line.params.size(), 3);
+  EXPECT_EQ(line.params[0], "index.nginx-debian.html");
+  EXPECT_EQ(line.params[1], "autoindex");
+  EXPECT_EQ(line.params[2], "on");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_END);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "index");
+  EXPECT_EQ(line.params.size(), 4);
+  EXPECT_EQ(line.params[0], "autoindex");
+  EXPECT_EQ(line.params[1], "on");
+  EXPECT_EQ(line.params[2], "index");
+  EXPECT_EQ(line.params[3], "nginx-debian.html");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "server");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_START);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_END);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "server");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_START);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "listen");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "8004");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "server_name");
+  EXPECT_EQ(line.params.size(), 1);
+  EXPECT_EQ(line.params[0], "test2");
+  EXPECT_EQ(line.type, config::SIMPLE);
+
+  EXPECT_TRUE(builder.GetNext(line));
+  EXPECT_EQ(line.name, "");
+  EXPECT_EQ(line.params.size(), 0);
+  EXPECT_EQ(line.type, config::BLOCK_END);
+
+  EXPECT_FALSE(builder.GetNext(line));
 }
